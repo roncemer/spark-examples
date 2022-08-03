071f2013b0cd51809d4c7305c70f23f6d5a83919
Ron Cemer <rcemer@rbcnotebook.local>
2022-08-03 08:51:58 -0500

    Sort events chronologically.

--- test_input/generators/flat_map_group_processing.php
+++ test_input/generators/flat_map_group_processing.php
@@ -1,14 +1,26 @@
 <?php
 
+function event_time_comparator($a, $b) {
+    $atime = strtotime($a->event_time);
+    $btime = strtotime($b->event_time);
+    return $atime-$btime;
+}
+
 $base_time = strtotime('2022-08-01 00:00:00 UTC');
+$events = [];
+for ($i = 0; $i < 1000; $i++) {
+    $events[] = (object)[
+        'event_time' => gmdate(DATE_ATOM, $base_time+mt_rand(0, 86399)),
+        'group_no' => mt_rand(1, 10),
+        'weight' => mt_rand(1, 1000000) * 0.0001,
+    ];
+}
+usort($events, 'event_time_comparator');
 
 $fp = fopen(dirname(__DIR__).'/flat_map_group_processing_events.csv.new', 'w');
 fputs($fp, "event_time,group_no,weight\n");
-for ($i = 0; $i < 1000; $i++) {
-    $event_time = gmdate(DATE_ATOM, $base_time+mt_rand(0, 86399));
-    $group_no = mt_rand(1, 10);
-    $weight = mt_rand(1, 1000000) * 0.0001;
-    fprintf($fp, "%s,%d,%.4f\n", $event_time, $group_no, $weight);
+foreach ($events as $event) {
+    fprintf($fp, "%s,%d,%.4f\n", $event->event_time, $event->group_no, $event->weight);
 }
 fclose($fp);
 
